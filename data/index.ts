22cbdd80fe94f541c6cbe674096f8128fd02b66a
BoomPaYe <110874107+BoomPaYe@users.noreply.github.com>
2024-11-11 13:03:06 +0700

    first commit

---
+++ data/index.ts
@@ -0,0 +1,208 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#recentprojects" },
+  { name: "Skills", link: "#skills" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "About Me ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[30vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/image.png",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I am a fourth-year university student.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Technologies.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Goals",
+    description: "I want to become a professional in this industry.",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Ted Toy Mobile Application",
+    des: "is an mobile application that allow user to swap their toys.",
+    img: "/phone mockup.png",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+    link: "/ui.earth.com",
+  },
+  {
+    id: 2,
+    title: "MAGNU5",
+    des: "Fashion Fusion.",
+    img: "/mangu5xtada.png",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+    link: "/ui.yoom.com",
+  },
+  {
+    id: 3,
+    title: "BorPort",
+    des: "A travel website application work as a team of 3 members.",
+    img: "/borport concept design.png",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+    link: "/ui.aiimg.com",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/figma-1-logo-svgrepo-com.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/aftereffect.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/illustrator.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/photoshop.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/js.svg",
+    nameImg: "/dockerName.svg",
+  },
+  {
+    id: 6,
+    name: "flutter",
+    img: "/flutter.svg",
+    nameImg: "/flutterName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Volunteer as Graphic Design",
+    desc: "Design a posters and stageboard.",
+    className: "md:col-span-2",
+    thumbnail: "/magnu5 white bg.png",
+  },
+  {
+    id: 2,
+    title: "Ted Toy Mobile Application UI",
+    desc: "Designed mobile app for both iOS & Android platforms using Figma.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/ted toy.png",
+  },
+  {
+    id: 3,
+    title: "Commercial Video of BorPort",
+    desc: "Editing while Adobe After effect and Adobe Illustrator.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/BorportMobile.png",
+  },
+  {
+    id: 4,
+    title: "BorPort Web Application UI",
+    desc: "Design an UI.",
+    className: "md:col-span-2",
+    thumbnail: "/BorportMobile.png",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    url:"https://github.com/BoomPaYe",
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+    url:"https://x.com/BoomPaYe10",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+    url:"https://www.linkedin.com/in/meach-pichmonireach-18a65b2aa/",
+  },
+];
